0823e93002aed74bba85527e986f14b33c60cc74
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-05-15 18:40:31 -0500

    added form component

---
+++ 01_Colt_React/src/01_basicsOfJSX.tsx
@@ -0,0 +1,21 @@
+// The basics of JSX, or TSX with TypeScript 
+
+/* 
+
+React uses components to render HTML 
+to the page, these are functions that 
+return HTML or JSX
+
+
+
+
+*/
+
+export default function InputForm() {
+    return (
+    <>
+    <p>Below is an input</p>
+    <input type="text" />
+    </>
+    )
+}
--- 01_Colt_React/src/main.tsx
+++ 01_Colt_React/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import InputForm from './01_basicsOfJSX.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <InputForm/>
   </React.StrictMode>,
 )
